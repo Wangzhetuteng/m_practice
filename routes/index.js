66858a6f7fb494288d394d68007a5a8aae3d8107
Wangzhetuteng <ericdicaprio@outlook.com>
2016-02-25 12:23:27 -0500

    added contact and about me pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express on Azure' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+	res.render('index', { title: 'sdf'});
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+	res.render('contact', { title: 'fdslahladshf'});
+});
+
 module.exports = router;
